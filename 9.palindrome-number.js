6aefcbb2cd666dbc694b32d827aae048dd1a8f4a
linchengzzz <1139489076@qq.com>
2018-11-28 15:22:37 +0800

    9th finish

---
+++ 9.palindrome-number.js
@@ -0,0 +1,52 @@
+/*
+ * [9] Palindrome Number
+ *
+ * https://leetcode-cn.com/problems/palindrome-number/description/
+ *
+ * algorithms
+ * Easy (52.87%)
+ * Total Accepted:    43.5K
+ * Total Submissions: 82.3K
+ * Testcase Example:  '121'
+ *
+ * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ *
+ * 示例 1:
+ *
+ * 输入: 121
+ * 输出: true
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: -121
+ * 输出: false
+ * 解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+ *
+ *
+ * 示例 3:
+ *
+ * 输入: 10
+ * 输出: false
+ * 解释: 从右向左读, 为 01 。因此它不是一个回文数。
+ *
+ *
+ * 进阶:
+ *
+ * 你能不将整数转为字符串来解决这个问题吗？
+ *
+ */
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if (x === 0) return true;
+    if (!x) return false;
+    let str = x.toString();
+    let str2 = str
+        .split('')
+        .reverse()
+        .join('');
+    return str === str2;
+};
